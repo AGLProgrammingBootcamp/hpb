2b6dbbeb2b3c4371dd94fcdaeb07da1a0e7e6dcb
shotaishida <sgpcmg@gmail.com>
2016-03-25 06:57:24 +0000

    middle of making post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/top'
-  #get
+  root 'home#top'
+  get '/new' => 'home#new'
+  post '/create'=> 'home#create'
+  get '/show'=>'home#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

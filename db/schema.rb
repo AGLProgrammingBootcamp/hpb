a174e0b63b17454589abe947cbe56a7619f40e1f
shotaishida <sgpcmg@gmail.com>
2016-03-26 06:26:22 +0000

    model recover

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326053438) do
+ActiveRecord::Schema.define(version: 20160326062254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "participants", force: :cascade do |t|
-    t.string   "participant_name"
-    t.string   "participant_email"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.string   "name"
+    t.string   "email"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|

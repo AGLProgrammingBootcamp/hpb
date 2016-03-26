ca10db7537a24b33b9b3577ca0d0e870c277039a
shotaishida <sgpcmg@gmail.com>
2016-03-26 03:11:52 +0000

    search done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
         @post=Post.new
     end
   
+    #本当はvalidationを使うべき
     def create
         #fail params.inspect
         @post=Post.new(post_params)
@@ -16,6 +17,25 @@ class PostsController < ApplicationController
         @post=Post.find(params[:id])
     end
     
+    def input
+    end
+        
+    def search_price
+        @sakes=Post.all
+        posts = Post.all
+        @posts = Array.new
+        posts.each do |post|
+            item = post.price >= params[:price1].to_i && post.price <= params[:price2].to_i
+            if item
+                @posts << post
+            end
+        end 
+        render :action => 'index' 
+    end
+    
+    def index
+         @posts=Post.find(params[:id])
+    end
     
     private
     
